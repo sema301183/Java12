1800e8f0b9dad3829e2562e8fc7f41cb4ceb0306
Sema test <ganzalovdmitriy@gmail.com>
2023-03-20 20:31:14 +0300

    .gitignore

---
+++ src/main/java/org/example/MovieManager.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class MovieManager {
+    private String[] movies = new String[0];
+    private int limit;
+
+
+    public MovieManager() {
+        this.limit = 9;
+
+    }
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+    public String[] findLast() {
+        int resultLenght;
+        if (movies.length < limit) {
+            resultLenght = movies.length;
+        } else {
+            resultLenght = limit;
+        }
+        String[] tmp = new String[resultLenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+
+        }
+        return tmp;
+    }
+
+
+}
